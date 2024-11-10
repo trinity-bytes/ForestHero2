8dbb11f2b5796d80a069b6c09ae2d45e0b6bded3
Mauricio Teran <mteranzavala@gmail.com>
2024-11-09 23:03:24 -0500

    clase aliado

--- ForestHero2/Aliado.h
+++ ForestHero2/Aliado.h
@@ -1,11 +1,29 @@
 #pragma once
 #include "Agente.h"
 
-class Aliado : Agente
+class Aliado : public Agente
 {
 private:
 
 public:
 	Aliado() : Agente(velocidad) { velocidad = 10; }
 	~Aliado() {}
+	void mover(Graphics^ g)
+	{
+		idX++;
+		if (idX > 3) idX = 0;
+
+		x += dx;
+		x -= dx;
+	}
+	void dibujar(Graphics^ g, Bitmap^ bmp)
+	{
+		System::Drawing::Rectangle seccionMostrada = System::Drawing::Rectangle(idX * ancho, idY * alto, ancho, alto);
+		System::Drawing::Rectangle zoom = System::Drawing::Rectangle(x, y, ancho * 1.0, alto * 1.0);
+		g->DrawImage(bmp, zoom, seccionMostrada, GraphicsUnit::Pixel);
+	}
+	System::Drawing::Rectangle getRectangle()
+	{
+		return System::Drawing::Rectangle(x, y, ancho * 0.03, alto * 0.03);
+	}
 };
