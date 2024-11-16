32c9edc6dc0f1536f1f945f640d8f812178265fe
Ixooc <vasquezllaveoscarlizandro@gmail.com>
2024-11-15 23:37:27 -0500

    ranking boton

--- ForestHero2/MenuJuego.h
+++ ForestHero2/MenuJuego.h
@@ -35,7 +35,9 @@ namespace ForestHero2 {
 			}
 		}
 	private: System::Windows::Forms::Panel^ panel1;
-	private: System::Windows::Forms::Button^ button1;
+	private: System::Windows::Forms::Button^ botonJugar;
+	private: System::Windows::Forms::Button^ botonRanking;
+
 	protected:
 
 	private:
@@ -53,7 +55,8 @@ namespace ForestHero2 {
 		{
 			System::ComponentModel::ComponentResourceManager^ resources = (gcnew System::ComponentModel::ComponentResourceManager(MenuJuego::typeid));
 			this->panel1 = (gcnew System::Windows::Forms::Panel());
-			this->button1 = (gcnew System::Windows::Forms::Button());
+			this->botonRanking = (gcnew System::Windows::Forms::Button());
+			this->botonJugar = (gcnew System::Windows::Forms::Button());
 			this->panel1->SuspendLayout();
 			this->SuspendLayout();
 			// 
@@ -61,31 +64,49 @@ namespace ForestHero2 {
 			// 
 			this->panel1->BackColor = System::Drawing::Color::Transparent;
 			this->panel1->BackgroundImage = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"panel1.BackgroundImage")));
-			this->panel1->Controls->Add(this->button1);
+			this->panel1->Controls->Add(this->botonRanking);
+			this->panel1->Controls->Add(this->botonJugar);
 			this->panel1->Location = System::Drawing::Point(0, 0);
 			this->panel1->Name = L"panel1";
 			this->panel1->Size = System::Drawing::Size(1366, 768);
 			this->panel1->TabIndex = 0;
 			// 
-			// button1
+			// botonRanking
 			// 
-			this->button1->BackColor = System::Drawing::Color::Transparent;
-			this->button1->BackgroundImage = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"button1.BackgroundImage")));
-			this->button1->BackgroundImageLayout = System::Windows::Forms::ImageLayout::None;
-			this->button1->Cursor = System::Windows::Forms::Cursors::Default;
-			this->button1->FlatAppearance->BorderSize = 0;
-			this->button1->FlatAppearance->MouseDownBackColor = System::Drawing::Color::Transparent;
-			this->button1->FlatAppearance->MouseOverBackColor = System::Drawing::Color::Transparent;
-			this->button1->FlatStyle = System::Windows::Forms::FlatStyle::Flat;
-			this->button1->Location = System::Drawing::Point(511, 317);
-			this->button1->Name = L"button1";
-			this->button1->RightToLeft = System::Windows::Forms::RightToLeft::No;
-			this->button1->Size = System::Drawing::Size(338, 132);
-			this->button1->TabIndex = 0;
-			this->button1->UseVisualStyleBackColor = false;
-			this->button1->Click += gcnew System::EventHandler(this, &MenuJuego::button1_Click);
-			this->button1->MouseEnter += gcnew System::EventHandler(this, &MenuJuego::button1_MouseEnter);
-			this->button1->MouseLeave += gcnew System::EventHandler(this, &MenuJuego::button1_MouseLeave);
+			this->botonRanking->BackgroundImage = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"botonRanking.BackgroundImage")));
+			this->botonRanking->BackgroundImageLayout = System::Windows::Forms::ImageLayout::None;
+			this->botonRanking->FlatAppearance->BorderSize = 0;
+			this->botonRanking->FlatAppearance->MouseDownBackColor = System::Drawing::Color::Transparent;
+			this->botonRanking->FlatAppearance->MouseOverBackColor = System::Drawing::Color::Transparent;
+			this->botonRanking->FlatStyle = System::Windows::Forms::FlatStyle::Flat;
+			this->botonRanking->Location = System::Drawing::Point(62, 629);
+			this->botonRanking->Name = L"botonRanking";
+			this->botonRanking->Size = System::Drawing::Size(235, 94);
+			this->botonRanking->TabIndex = 1;
+			this->botonRanking->UseVisualStyleBackColor = false;
+			this->botonRanking->Click += gcnew System::EventHandler(this, &MenuJuego::botonRanking_Click);
+			this->botonRanking->MouseEnter += gcnew System::EventHandler(this, &MenuJuego::botonRanking_MouseEnter);
+			this->botonRanking->MouseLeave += gcnew System::EventHandler(this, &MenuJuego::botonRanking_MouseLeave);
+			// 
+			// botonJugar
+			// 
+			this->botonJugar->BackColor = System::Drawing::Color::Transparent;
+			this->botonJugar->BackgroundImage = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"botonJugar.BackgroundImage")));
+			this->botonJugar->BackgroundImageLayout = System::Windows::Forms::ImageLayout::None;
+			this->botonJugar->Cursor = System::Windows::Forms::Cursors::Default;
+			this->botonJugar->FlatAppearance->BorderSize = 0;
+			this->botonJugar->FlatAppearance->MouseDownBackColor = System::Drawing::Color::Transparent;
+			this->botonJugar->FlatAppearance->MouseOverBackColor = System::Drawing::Color::Transparent;
+			this->botonJugar->FlatStyle = System::Windows::Forms::FlatStyle::Flat;
+			this->botonJugar->Location = System::Drawing::Point(511, 317);
+			this->botonJugar->Name = L"botonJugar";
+			this->botonJugar->RightToLeft = System::Windows::Forms::RightToLeft::No;
+			this->botonJugar->Size = System::Drawing::Size(338, 132);
+			this->botonJugar->TabIndex = 0;
+			this->botonJugar->UseVisualStyleBackColor = false;
+			this->botonJugar->Click += gcnew System::EventHandler(this, &MenuJuego::botonJugar_Click);
+			this->botonJugar->MouseEnter += gcnew System::EventHandler(this, &MenuJuego::botonJugar_MouseEnter);
+			this->botonJugar->MouseLeave += gcnew System::EventHandler(this, &MenuJuego::botonJugar_MouseLeave);
 			// 
 			// MenuJuego
 			// 
@@ -104,15 +125,24 @@ namespace ForestHero2 {
 
 		}
 #pragma endregion
-	private: System::Void button1_Click(System::Object^ sender, System::EventArgs^ e) {
+	private: System::Void botonJugar_Click(System::Object^ sender, System::EventArgs^ e) {
 		MyForm^ MyFormJuego = gcnew MyForm();
 		MyFormJuego->Show();
 	}
-	private: System::Void button1_MouseEnter(System::Object^ sender, System::EventArgs^ e) {
-		button1->BackgroundImage = Image::FromFile("Resources/Buttons/menuPrincipal-assets/jugarblanco.png");
+	private: System::Void botonJugar_MouseEnter(System::Object^ sender, System::EventArgs^ e) {
+		botonJugar->BackgroundImage = Image::FromFile("Resources/Buttons/menuPrincipal-assets/jugarblanco.png");
 	}
-	private: System::Void button1_MouseLeave(System::Object^ sender, System::EventArgs^ e) {
-		button1->BackgroundImage = Image::FromFile("Resources/Buttons/menuPrincipal-assets/jugarcopy.png");
+	private: System::Void botonJugar_MouseLeave(System::Object^ sender, System::EventArgs^ e) {
+		botonJugar->BackgroundImage = Image::FromFile("Resources/Buttons/menuPrincipal-assets/jugarcopy.png");
 	}
-	};
+	private: System::Void botonRanking_Click(System::Object^ sender, System::EventArgs^ e) {
+
+	}
+private: System::Void botonRanking_MouseEnter(System::Object^ sender, System::EventArgs^ e) {
+	botonRanking->BackgroundImage = Image::FromFile("Resources/Buttons/menuPrincipal-assets/rankingblanco.png");
+}
+private: System::Void botonRanking_MouseLeave(System::Object^ sender, System::EventArgs^ e) {
+	botonRanking->BackgroundImage = Image::FromFile("Resources/Buttons/menuPrincipal-assets/rankingcopy.png");
+}
+};
 }
